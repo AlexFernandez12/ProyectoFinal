6ca3c0a42c02095d35e2a1c41cd3c5f61d7c95a7
Alex <afd0001@alu.medac.es>
2022-04-29 22:57:55 +0200

    TFG1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './Login'
+import Login from './components/Login'
  
  
 export default function App(){
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './login.css';
+import './css/login.css';
 import reactDom from 'react-dom';
  
 
