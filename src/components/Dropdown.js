153dcf414912af9d8eb6041608fafa2d2099a1ae
Alex <alexfernandezduarte@gmail.com>
2022-05-13 09:13:06 +0200

    menu

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -10,6 +10,10 @@ function Dropdown() {
 
   return (
     <>
+     <ul
+        onClick={handleClick}
+        className={click ? 'dropdown-menu clicked' : 'dropdown-menu'}
+      >
         {MenuItems.map((item, index) => {
           return (
             <li key={index}>
@@ -23,6 +27,7 @@ function Dropdown() {
             </li>
           );
         })}
+         </ul>
     </>
   );
 }
