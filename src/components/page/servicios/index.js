30fdbc86606e6615e6f2c54599e283ac69f3025e
Alex <alexfernandezduarte@gmail.com>
2022-05-13 10:06:35 +0200

    footer

--- src/components/page/servicios/index.js
+++ src/components/page/servicios/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import emailjs from 'emailjs-com';
 import { Link } from "react-router-dom";
 import Portada from "images/inicio.jpg";
 
 export default function Servicios() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  } 
     return (
         <div className="inicio">   
-               <form class="form">
+               <form class="form" onSubmit={sendEmail}>
   <h2 className='tit'>CONTACT US</h2>
   <p type="Name:"><input placeholder="Escriba su nombre" className='cuadro'></input></p>
   <p type="Email:"><input placeholder="Escriba su email" className='cuadro'></input></p>
