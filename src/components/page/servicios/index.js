3cde38115fd83a83a3892566f4331eff7048bff8
Alex <alexfernandezduarte@gmail.com>
2022-05-16 08:49:13 +0200

    email_correcto

--- src/components/page/servicios/index.js
+++ src/components/page/servicios/index.js
@@ -1,25 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import emailjs from 'emailjs-com';
 
 export default function Servicios() {
+  const form = useRef();
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('service_6z9klu3', 'template_0xvjt49', e.target, 'y--EKdEHLNw4fMDW4')
+    emailjs.sendForm('service_6z9klu3', 'template_0xvjt49', form.current, 'y--EKdEHLNw4fMDW4')
       .then((result) => {
           console.log(result.text);
+          alert ("Petición enviada correctamente");
       }, (error) => {
           console.log(error.text);
       });
   } 
     return (
         <div className="inicio">   
-               <form class="form_contacto" onSubmit={sendEmail}>
+               <form  ref={form} class="form_contacto" onSubmit={sendEmail}>
   <h2 className='tit'>CONTACT US</h2>
-  <p type="Name:"><input placeholder="Escriba su nombre" className='cuadro'></input></p>
-  <p type="Email:"><input placeholder="Escriba su email" className='cuadro'></input></p>
-  <p type="Message:"><input placeholder="Cuentenos su problema" className='cuadro'></input></p>
-  <button className='enviar_mensaje'>Enviar</button>
+  <p type="Name:"><input placeholder="Escriba su nombre" className='cuadro' name='name'></input></p>
+  <p type="Email:"><input placeholder="Escriba su email" className='cuadro' name='subject'></input></p>
+  <p type="Message:"><input placeholder="Cuentenos su problema" className='cuadro' name='email'></input></p>
+  <input type="submit" value="Send" className='enviar_mensaje' />
 
   <div className='contactar'>
     <span class="fa fa-phone"></span>623 714 936
