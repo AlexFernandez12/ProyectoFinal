1eed141cdf22dfe8399be4695bc31ea550e3821f
SimonDuque <82675363+SimonDuque@users.noreply.github.com>
2022-05-27 20:03:12 +0200

    pagina de inicio terminada

--- src/components/page/inicio/index.jsx
+++ src/components/page/inicio/index.jsx
@@ -6,16 +6,27 @@ import img1 from '../../../images/handsome.jpg';
 import img2 from '../../../images/mujer.jpg';
 import img3 from '../../../images/ninos.jpg';
 import img4 from '../../../images/pareja.jpg';
+import headgif from '../../../images/descuento.svg';
+import pago from '../../../images/pagoseguro.svg';
+import devolucion from '../../../images/devolucion.svg';
+import recogida from '../../../images/recogida.svg';
 import img5 from '../../../images/inicio.jpg';
 import { Link } from "react-router-dom";
 
 
 const D = () => {
 	return (
-    
+
 		<main>
       {/* <img src={img5} alt=""/> */}
-      <div className='slidefotos'>
+	  <div className='headgif'>
+	  		<img src={headgif} alt="" />
+			  <h2 className='envio'>Envío gratis en pedidos de +70€ &nbsp;</h2>
+			  
+			  <h4 className='envio2'>ENVÍO 24-48H</h4>
+	  </div>
+	  
+      <div className='slidefotos'>		  
 			<Titulo>Categorías</Titulo>
 			<Slideshow controles={true}>
 				<Slide>
@@ -52,6 +63,32 @@ const D = () => {
 				</Slide>
 			</Slideshow>
       </div>
+
+	  <div className='tarjeta1'>
+			<div className='bodytarjeta'>
+				<img className='fotopago' src={pago} alt="" />
+				<h2 className='titulotarjeta'>PAGO SEGURO</h2>
+				<p className='descripciontarjeta'><b>Pago contra reembolso, con tarjeta, Bizum o mediante transferencia bancaria.</b></p>
+			</div>
+		</div> 
+
+		
+		<div className='tarjeta2'>
+			<div className='bodytarjeta2'>
+				<img className='fotopago2' src={devolucion} alt="" />
+				<h2 className='titulotarjeta2'>DEVOLUCIONES</h2>
+				<p className='descripciontarjeta2'><b>Devoluciones gratuitas en tienda durante 30 días.</b></p>
+			</div>
+		</div>
+
+		<div className='tarjeta3'>
+			<div className='bodytarjeta3'>
+				<img className='fotopago3' src={recogida} alt="" />
+				<h2 className='titulotarjeta3'>RECOGIDA</h2>
+				<p className='descripciontarjeta3'><b>Recogida gratuita en tienda.</b></p>
+			</div>
+		</div> 
+
 		</main>
 	);
 }
